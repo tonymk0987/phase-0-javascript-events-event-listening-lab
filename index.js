a26e7cc780589d0d1a79f2c7b6de31d8e6e4f143
tonymk0987 <k.tony.muriuki@gmail.com>
2024-04-01 17:15:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const button = document.getElementById("button"); 
+    button.addEventListener("click", function() {
+        console.log("Button clicked!");
+    });
 }
